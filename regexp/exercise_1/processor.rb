ae29af6131faa5182db6ee159cd79431f55e499b
Mike Chlipala <mike.chlipala@livingsocial.com>
2012-08-11 14:59:59 -0400

    finished example1 exercises

--- regexp/exercise_1/processor.rb
+++ regexp/exercise_1/processor.rb
@@ -7,6 +7,7 @@ class SingleCharacterMatcher
 
   def self.contains? char, string
     /^[^#{char}]*#{char}[^#{char}]*$/ =~ string
+     # /(^#{char}[^#{char}]+$|^[^#{char}]#{char}{1}[^#{char}]$|^[^#{char}]#{char}{0}#{char}$)/ =~ string
   end
 
   def self.contains_only_this_character? char, string
@@ -24,4 +25,22 @@ class CharacterSequenceMatcher
     /(\S)(\1+)/ =~ chars
   end
 
+end
+
+class VowlerPairMatcher
+  def self.check chars
+    /([aeiou][\\n]*[aeiou])/i =~ chars
+  end
+end
+
+class RepetitionMatcher
+  def self.check chars
+    /(\S)(\1+)/i =~ chars
+  end
+
+  def self.last string
+    string.gsub!(/(?:\\n)|[\W]/, "")
+    matches = string.scan(/(\w)(\1+)/i)
+    matches.last.join
+  end
 end
